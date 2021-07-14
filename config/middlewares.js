bae312d3440f90dd3ac2cfae014f376542304974
enzomazocorodrigues <enzo.rodrigues2@gmail.com>
2021-07-14 12:22:24 -0300

    Correção de CORS

--- config/middlewares.js
+++ config/middlewares.js
@@ -4,4 +4,13 @@ const cors = require('cors');
 module.exports = app => {
   app.use(bodyParser.json());
   app.use(cors());
+
+  app.use(function(req, res, next) {
+    // res.header("Access-Control-Allow-Origin", ['http://localhost:8080', 'https://typefast-v1.vercel.app']); 
+    res.header("Access-Control-Allow-Origin", '*'); // deletar
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+  });
 };
