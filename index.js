0168f5e1b9aba6dae2c2cf24f9027d625c9d478a
enzomazocorodrigues <enzo.rodrigues2@gmail.com>
2021-07-14 12:26:01 -0300

    Correção de bugs

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const app = require('express')()
 const consign = require('consign');
 const db = require('./config/db')
-
 app.db = db
+
+app.use(function(req, res, next) {
+  // res.header("Access-Control-Allow-Origin", ['http://localhost:8080', 'https://typefast-v1.vercel.app']); 
+  res.header("Access-Control-Allow-Origin", '*'); // deletar
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 consign()
   .include('./config/passport.js')
